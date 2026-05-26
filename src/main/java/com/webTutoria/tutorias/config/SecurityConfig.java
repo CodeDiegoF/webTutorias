d3969760159fd9a041bb881045d748dc9e0fd659
Diego Fernández Arenas <dferare1310@g.educaand.es>
2026-05-26 13:39:28 +0200

    Actualizar configuración de seguridad: permitir acceso a nuevas rutas y recursos estáticos

--- src/main/java/com/webTutoria/tutorias/config/SecurityConfig.java
+++ src/main/java/com/webTutoria/tutorias/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
+                                "/error",
                                 "/auth/**",
                                 "/horarios/**",
                                 "/reservas/**",
